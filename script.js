944fc06925396d21863f70e87d9401da27b16c00
Simon Zettergren <simon.zettergren@elev.ga.ntig.se>
2024-02-22 14:35:40 +0100

    shopping cart display

--- script.js
+++ script.js
@@ -21,16 +21,45 @@ function addProduct(name, price) {
   for (let i = 0; i < shopping_cart.length; i++) {
     if (shopping_cart[i].name === name) {
       shopping_cart[i].amount += 1;
-      alert(JSON.stringify(shopping_cart));
+      updateShoppingCartHtml();
       return;
     }
   }
 
-  let newProduct = {
+  const newProduct = {
     name: name,
     price: price,
     amount: 1,
   };
   shopping_cart.push(newProduct);
-  alert(JSON.stringify(shopping_cart));
+  updateShoppingCartHtml();
+}
+
+let products = document.querySelector(".shopping-cart_products");
+
+function updateShoppingCartHtml() {
+  products.innerHTML = "";
+
+  for (let i = 0; i < shopping_cart.length; i++) {
+    const product = shopping_cart[i];
+
+    const amount_html = document.createElement("p");
+    amount_html.innerHTML = "(" + product.amount + ")";
+    amount_html.classList.add("shopping-cart_amount");
+
+    const name_html = document.createElement("p");
+    name_html.innerHTML = product.name;
+    name_html.classList.add("shopping-cart_name");
+
+    const price_html = document.createElement("p");
+    price_html.innerHTML = product.price * product.amount + " kr";
+    price_html.classList.add("shopping-cart_price");
+
+    const product_html = document.createElement("div");
+    product_html.appendChild(amount_html);
+    product_html.appendChild(name_html);
+    product_html.appendChild(price_html);
+
+    products.appendChild(product_html);
+  }
 }
